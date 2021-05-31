7762df5035d0a727cfddfa52240537d7916035d6
dleont <dleont@est.ups.edu.ec>
2021-05-31 01:49:02 -0500

    Pagina de Inicio
    Index.html

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -12,6 +12,6 @@ $conn->set_charset("utf8");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    /*echo "<p>Conexión exitosa!! :)</p>";*/
+    // echo "<p>Conexión exitosa!! :)</p>";
 }
 ?>
